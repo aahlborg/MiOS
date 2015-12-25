2b5c1ace846c5abd7b60d89fd01610f93258f122
Marcus Ahlberg <marcus@aahlborg.se>
2015-12-26 00:56:08 +0100

    Register empty interrupt handlers

---
+++ src/interrupts.c
@@ -0,0 +1,33 @@
+#include <inc/uart.h>
+
+void kernel_panic(void) __attribute__ ((noreturn));
+
+void __attribute__((interrupt("UNDEF"))) illegal_instruction_isr(void)
+{
+  uart_write('O');
+  kernel_panic();
+}
+
+void __attribute__((interrupt("SWI"))) software_interrupt_isr(void)
+{
+  uart_write('S');
+  kernel_panic();
+}
+
+void __attribute__((interrupt("ABORT"))) prefetch_abort_isr(void)
+{
+  uart_write('P');
+  kernel_panic();
+}
+
+void __attribute__((interrupt("IRQ"))) interrupt_isr(void)
+{
+  uart_write('Q');
+  kernel_panic();
+}
+
+void __attribute__((interrupt("FIQ"))) fast_interrupt_isr(void)
+{
+  uart_write('F');
+  kernel_panic();
+}
--- src/kernel_main.c
+++ src/kernel_main.c
@@ -8,6 +8,12 @@ void _kernel_main(unsigned int r0, unsigned int r1, unsigned int atags)
 {
   gpio_pin_set_function(GPIO_PIN_BOARD_LED, GPIO_FUNC_OUT);
   uart_init(115200, 8);
+
+  printf("MiOS kernel boot\r\n");
+  printf("r0: 0x%08x\r\n", r0);
+  printf("r1: 0x%08x\r\n", r1);
+  printf("atags: 0x%08x\r\n", atags);
+
   for (;;)
   {
     gpio_pin_write(GPIO_PIN_BOARD_LED, 1);
@@ -16,5 +22,17 @@ void _kernel_main(unsigned int r0, unsigned int r1, unsigned int atags)
     gpio_pin_write(GPIO_PIN_BOARD_LED, 0);
     printf("LED off\r\n");
     wait_us(500000);
+  }
+}
+
+void __attribute__ ((noreturn)) kernel_panic(void)
+{
+  // Flash board LED at 5 Hz
+  for (;;)
+  {
+    gpio_pin_write(GPIO_PIN_BOARD_LED, 1);
+    wait_us(100000);
+    gpio_pin_write(GPIO_PIN_BOARD_LED, 0);
+    wait_us(100000);
   }
 }
